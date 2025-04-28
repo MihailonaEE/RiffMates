95fa3f3222f6fa496289e3062d0adabd9933fd8a
MihailonaEE <noraeem@icloud.com>
2025-04-28 22:22:35 +0700

    add news page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
 def credits(request):
     content = "Nicky\nEleonora"
     return HttpResponse(content, content_type="text/plain")
+
+def news(request):
+    data = {"news":[
+        "RiffMates now has a news page!",
+        "RiffMates has its first page"
+    ]}
+    return render(request, "news.html", data)
